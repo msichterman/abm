1ac24488bde0f7b9ec4dfe54cc8b5b2db46df621
Matt Sichterman <msichterman1@gmail.com>
2022-01-04 17:27:34 -0500

    Begin sanity setup

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
+const ADMIN_REWRITE = {
+  source: "/admin/:path*",
+  destination:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3333/admin/:path*"
+      : "/admin/index.html",
+};
+
 module.exports = {
   images: {
     domains: [
-      'i.scdn.co', // Spotify Album Art
       'pbs.twimg.com', // Twitter Profile Picture
     ],
   },
+  rewrites: () => [ADMIN_REWRITE],
 }
